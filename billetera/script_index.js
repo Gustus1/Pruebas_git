60b8268304e2d6369df742f02046f59c4c5885e5
Gustavo Sanhueza <gussanhuez.h@gmail.com>
2025-11-21 00:07:24 -0300

    Subida aplicacion billetera

---
+++ billetera/script_index.js
@@ -0,0 +1,8 @@
+function iniciar_sesion(){
+    email = document.getElementById("email")
+    password = document.getElementById("password")
+
+    console.log(email)
+    console.log(password)
+
+}
